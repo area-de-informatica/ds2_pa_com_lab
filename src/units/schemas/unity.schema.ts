95322abd4683a2667a2ab995425524295af26ec0
AndresSaidPardeyPabon <apardeypabon54@correo.unicordoba.edu.co>
2025-05-27 07:28:33 -0500

    Modifiqued

---
+++ src/units/schemas/unity.schema.ts
@@ -0,0 +1,38 @@
+//schemas/unity.schema.ts
+
+
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from 'mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+    timestamps: true
+})
+
+export class Unity extends Document{
+    @Prop()
+    username: string;
+
+    @Prop({ unique: [true, 'Email already exists'] })
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop({default: 'guest'})
+    role: string;
+
+    @Prop()
+    status: string;
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    picture: string;
+
+    @Prop()
+    updatedAt: Date;    
+}
+
+export const UserSchema = SchemaFactory.createForClass(Unity);
